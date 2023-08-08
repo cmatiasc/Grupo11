743cb5979e1d0152021d1f03e0cbf6241b11009f
YoNahum <nahumrecalde@yahoo.com.ar>
2023-08-08 11:56:58 -0300

    Update Clase_de_cosas_locas.java

--- src/grupo11/Clase_de_cosas_locas.java
+++ src/grupo11/Clase_de_cosas_locas.java
@@ -4,7 +4,7 @@ package grupo11;
 
 public class Clase_de_cosas_locas {
     
-    public static frase{
-            System.out.println("Qué copado que arranqué esta parte entendiendo!");
-    }
+    public static void frase(){
+        System.out.println("Qué copado que arranqué esta parte entendiendo!");
+    }    
 }
