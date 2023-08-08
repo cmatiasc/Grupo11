7837aae4107c465b25d2047dca49e7c7972bd334
cmatiasc <guiamultimedia1989@gmail.com>
2023-08-08 11:12:22 -0300

    puse mi nombre

--- src/grupo11/Grupo11.java
+++ src/grupo11/Grupo11.java
@@ -17,6 +17,8 @@ public class Grupo11 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola, soy Nahum!");
+        System.out.println("soy matias");
+        
     }
     
 }
